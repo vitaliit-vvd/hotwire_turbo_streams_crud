3b3224de02fee2823cc1f5759ca629e0e698602d
vitaliit-vvd <vitalii.t@vevidi.com>
2023-11-04 02:46:46 +0200

    update messages counter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,7 +40,10 @@ class MessagesController < ApplicationController
                                 locals: { message: Message.new }),
             turbo_stream.prepend('messages',
                                  partial: 'messages/message',
-                                 locals: { message: @message })
+                                 locals: { message: @message }),
+            turbo_stream.update('message_counter', Message.count)
+            # turbo_stream.update('message_counter', html: Message.count)
+            # turbo_stream.update('message_counter', html: "#{Message.count}")
           ]
         end
         format.html { redirect_to message_url(@message), notice: 'Message was successfully created.' }
@@ -80,7 +83,12 @@ class MessagesController < ApplicationController
     @message.destroy!
 
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove(@message) }
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.remove(@message),
+          turbo_stream.update('message_counter', Message.count)
+        ]
+      end
       # format.turbo_stream { render turbo_stream: turbo_stream.remove("message_#{@message.id}") }
       format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
       format.json { head :no_content }
